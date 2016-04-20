c640fe8aea290d3c8b937affaffa8732b377389e
daboosteel <sammer121@yahoo.com>
2016-04-21 03:35:10 +0500

    Update About.aspx.cs

--- About.aspx.cs
+++ About.aspx.cs
@@ -9,6 +9,9 @@ public partial class About : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+  
+  
+             Response.write("hello");
+             
     }
 }
